d6f032c0a05431c783475034cbfa29541391156e
ekalcrench <hhaekalazmi@gmail.com>
2022-06-07 13:34:43 +0700

    Mengubah authConfig

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,10 +3,11 @@ export const msalConfig = {
     clientId: "f9ab0a06-4349-48b5-860a-e381b80ae30c",
     authority: "https://login.microsoftonline.com/consumers", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
     redirectUri: "https://gray-flower-0a2d93e00.1.azurestaticapps.net",
+    postLogoutRedirectUri: "https://gray-flower-0a2d93e00.1.azurestaticapps.net",
   },
   cache: {
     cacheLocation: "sessionStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    storeAuthStateInCookie: true, // Set this to "true" if you are having issues on IE11 or Edge
   },
 };
 
